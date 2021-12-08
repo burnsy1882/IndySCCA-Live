2cd08d19d86399ba8da1b77ab3a598eab79cd40c
burns <burns@IT-01.local>
2021-12-08 11:57:44 -0500

    Updated region image

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
             <link rel="stylesheet" href="stylesheet.css">
 
-            <script src="#"></script>
+            <!-- <script src="#"></script> -->
         </head>
 
         <body>
