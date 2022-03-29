570721fd24824a9cfb02a25bac91fc51d1e96ec2
Kevin Burns <burnsy1882@gmail.com>
2022-03-29 11:22:28 -0400

    Updated vertical class list.

--- classes-vertical.php
+++ classes-vertical.php
@@ -1,161 +1,97 @@
 <div class="w3-responsive">
-    <table style="width:90%" id="tbl01" class="w3-table w3-striped w3-border w3-bordered w3-centered">
+    <table id="tbl01" class="w3-table w3-striped w3-border w3-bordered w3-centered">
         <thead>
             <tr class="w3-black">
-                <th>STREET</th>
-                <th>STREET TOURING</th>
-                <th>STREET PREPARED</th>
-                <th>STREET MODIFIED</th>
-                <th>PREPARED</th>
-                <th>MODIFIED</th>
-                <th>KART/FSAE</th>
-                <th>SPEC</th>
-                <th>CAM</th>
-                <th>HERITAGE</th>
-                <th>XTREME</th>
-                <th>PRO</th>
+                <th>GROUP 1 - STREET</th>
+                <th>GROUP 1</th>
+                <th>GROUP 2</th>
+                <th>GROUP 3</th>
             </tr>
         </thead>
         <tbody>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("SSR"); ?></td>
-                <td><?php echo classexists("STH"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("SSM"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("FSAE"); ?></td>
-                <td><?php echo classexists("SSC"); ?></td>
-                <td><?php echo classexists("CAMC"); ?></td>
-                <td><?php echo classexists("HCS"); ?></td>
-                <td><?php echo classexists("XSA"); ?></td>
-                <td><?php echo classexists("X"); ?></td>
+                <td><?php echo classexists("1SCL"); ?></td>
+                <td><?php echo classexists("1CL"); ?></td>
+                <td><?php echo classexists("2CL"); ?></td>
+                <td><?php echo classexists("3CL"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("SS"); ?></td>
-                <td><?php echo classexists("STR"); ?></td>
-                <td><?php echo classexists("SSP"); ?></td>
-                <td><?php echo classexists("SM"); ?></td>
-                <td><?php echo classexists("XP"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("KM"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("CAMS"); ?></td>
-                <td><?php echo classexists("HCR"); ?></td>
-                <td><?php echo classexists("XSB"); ?></td>
-                <td><?php echo classexists("ZF"); ?></td>
+                <td><?php echo classexists("1SA"); ?></td>
+                <td><?php echo classexists("1A"); ?></td>
+                <td><?php echo classexists("2A"); ?></td>
+                <td><?php echo classexists("3A"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("AS"); ?></td>
-                <td><?php echo classexists("STS"); ?></td>
-                <td><?php echo classexists("ASP"); ?></td>
-                <td><?php echo classexists("SMF"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("AM"); ?></td>
-                <td><?php echo classexists("JA"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("CAMT"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SB"); ?></td>
+                <td><?php echo classexists("1B"); ?></td>
+                <td><?php echo classexists("2B"); ?></td>
+                <td><?php echo classexists("3B"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("BS"); ?></td>
-                <td><?php echo classexists("STU"); ?></td>
-                <td><?php echo classexists("BSP"); ?></td>
-                <td><?php echo classexists("SMS"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("BM"); ?></td>
-                <td><?php echo classexists("JB"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SC"); ?></td>
+                <td><?php echo classexists("1C"); ?></td>
+                <td><?php echo classexists("2C"); ?></td>
+                <td><?php echo classexists("3C"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("CS"); ?></td>
-                <td><?php echo classexists("STX"); ?></td>
-                <td><?php echo classexists("CSP"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("CP"); ?></td>
-                <td><?php echo classexists("CM"); ?></td>
-                <td><?php echo classexists("JC"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SD"); ?></td>
+                <td><?php echo classexists("1D"); ?></td>
+                <td><?php echo classexists("2D"); ?></td>
+                <td><?php echo classexists("3D"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("DS"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("DSP"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("DP"); ?></td>
-                <td><?php echo classexists("DM"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SE"); ?></td>
+                <td><?php echo classexists("1E"); ?></td>
+                <td><?php echo classexists("2E"); ?></td>
+                <td><?php echo classexists("3E"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("ES"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("ESP"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("EP"); ?></td>
-                <td><?php echo classexists("EM"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SF"); ?></td>
+                <td><?php echo classexists("1F"); ?></td>
+                <td><?php echo classexists("2F"); ?></td>
+                <td><?php echo classexists("3F"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("FS"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("FSP"); ?></td>
-                <td>&nbsp;</td>
-                <td><?php echo classexists("FP"); ?></td>
-                <td><?php echo classexists("FM"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SG"); ?></td>
+                <td><?php echo classexists("1G"); ?></td>
+                <td><?php echo classexists("2G"); ?></td>
+                <td><?php echo classexists("3G"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("GS"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SH"); ?></td>
+                <td><?php echo classexists("1H"); ?></td>
+                <td><?php echo classexists("2H"); ?></td>
+                <td><?php echo classexists("3H"); ?></td>
             </tr>
             <tr style="white-space: nowrap;">
-                <td><?php echo classexists("HS"); ?></td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><?php echo classexists("1SJ"); ?></td>
+                <td><?php echo classexists("1J"); ?></td>
+                <td><?php echo classexists("2J"); ?></td>
+                <td><?php echo classexists("3J"); ?></td>
+            </tr>
+            <tr style="white-space: nowrap;">
+                <td><?php echo classexists("1SK"); ?></td>
+                <td><?php echo classexists("1K"); ?></td>
+                <td><?php echo classexists("2K"); ?></td>
+                <td><?php echo classexists("3K"); ?></td>
+            </tr>
+            <tr style="white-space: nowrap;">
+                <td><?php echo classexists("1SM"); ?></td>
+                <td><?php echo classexists("1M"); ?></td>
+                <td><?php echo classexists("2M"); ?></td>
+                <td><?php echo classexists("3M"); ?></td>
+            </tr>
+            <tr style="white-space: nowrap;">
+                <td><?php echo classexists("1SN"); ?></td>
+                <td><?php echo classexists("1N"); ?></td>
+                <td><?php echo classexists("2N"); ?></td>
+                <td><?php echo classexists("3N"); ?></td>
+            </tr>
+            <tr style="white-space: nowrap;">
+                <td><?php echo classexists("1SP"); ?></td>
+                <td><?php echo classexists("1P"); ?></td>
+                <td><?php echo classexists("2P"); ?></td>
+                <td><?php echo classexists("3P"); ?></td>
             </tr>
         </tbody>
     </table>
