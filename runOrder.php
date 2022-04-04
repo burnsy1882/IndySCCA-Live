4366c76e01fd7501a15600c459552f90e55ec448
Kevin Burns <burnsy1882@gmail.com>
2022-04-04 08:06:26 -0400

    Update runOrder.php

--- runOrder.php
+++ runOrder.php
@@ -1,11 +1,15 @@
 <?php
-    global $heats;
-    $heats = 0;
+/**
+ * heats
+ *
+ * Variable to count number of heats generated from tsAdmin
+ */
+$heats = 0;
 
-    foreach (glob("./files/Heat[0-9]RunOrder.html") as $file)
-    {
-        $heats++;
-    }
+foreach (glob("./files/Heat[0-9]RunOrder.html") as $file)
+{
+    $heats++;
+}
 ?>
 <div class="w3-responsive">
     <table id="tbl-runOrder" class="w3-table w3-striped w3-border w3-bordered w3-centered">
@@ -18,16 +22,22 @@
 
         <tbody>
             <?php
-                for ($i = 1; $i <= $heats; $i++)
-                //foreach ($heats as $heat)
+            for ($i = 1; $i <= $heats; $i++)
+            {
+                echo "<tr>";
+                echo "<td>$i</td>";
+                $file = file_get_contents("./files/Heat".$i."RunOrder.html");
+                echo "<td>$file</td>";
+                if (is_readable("./files/GridSheets".$i.".html"))
                 {
-                    echo "<tr>";
-                    echo "<td>$i</td>";
-                    $file = file_get_contents("./files/Heat".$i."RunOrder.html");
-                    echo "<td>$file</td>";
-                    if (is_readable("./files/GridSheets".$i.".html")) {echo '<td><a href="./files/GridSheets'.$i.'.html">GridSheets</a></td>';} else {echo "<td>&nbsp;</td>";}
-                    echo "</tr>";
+                    echo '<td><a href="./files/GridSheets'.$i.'.html">GridSheets</a></td>';
                 }
+                else
+                {
+                    echo "<td>&nbsp;</td>";
+                }
+                echo "</tr>";
+            }
             ?>
         </tbody>
     </table>
