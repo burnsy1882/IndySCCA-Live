57f234a084f4a789c86c1512c7283eeb69580a65
Kevin Burns <kburns@solential.com>
2022-04-21 16:15:39 -0400

    Adding combined classes

--- classes-horizontal.php
+++ classes-horizontal.php
@@ -6,14 +6,18 @@
                 <td><?php echo classExists("1SCL"); ?></td>
                 <td><?php echo classExists("1SA"); ?></td>
                 <td><?php echo classExists("1SB"); ?></td>
+                <td><?php echo classExists("1SAB"); ?></td>
                 <td><?php echo classExists("1SC"); ?></td>
                 <td><?php echo classExists("1SD"); ?></td>
+                <td><?php echo classExists("1SCD"); ?></td>
                 <td><?php echo classExists("1SE"); ?></td>
                 <td><?php echo classExists("1SF"); ?></td>
                 <td><?php echo classExists("1SG"); ?></td>
+                <td><?php echo classExists("1SFG"); ?></td>
                 <td><?php echo classExists("1SH"); ?></td>
                 <td><?php echo classExists("1SJ"); ?></td>
                 <td><?php echo classExists("1SK"); ?></td>
+                <td><?php echo classExists("1SJK"); ?></td>
                 <td><?php echo classExists("1SM"); ?></td>
                 <td><?php echo classExists("1SMM"); ?></td>
                 <td><?php echo classExists("1SN"); ?></td>
@@ -24,14 +28,18 @@
                 <td><?php echo classExists("1CL"); ?></td>
                 <td><?php echo classExists("1A"); ?></td>
                 <td><?php echo classExists("1B"); ?></td>
+                <td><?php echo classExists("1AB"); ?></td>
                 <td><?php echo classExists("1C"); ?></td>
                 <td><?php echo classExists("1D"); ?></td>
+                <td><?php echo classExists("1CD"); ?></td>
                 <td><?php echo classExists("1E"); ?></td>
                 <td><?php echo classExists("1F"); ?></td>
                 <td><?php echo classExists("1G"); ?></td>
+                <td><?php echo classExists("1FG"); ?></td>
                 <td><?php echo classExists("1H"); ?></td>
                 <td><?php echo classExists("1J"); ?></td>
                 <td><?php echo classExists("1K"); ?></td>
+                <td><?php echo classExists("1JK"); ?></td>
                 <td><?php echo classExists("1M"); ?></td>
                 <td><?php echo classExists("1MM"); ?></td>
                 <td><?php echo classExists("1N"); ?></td>
@@ -42,14 +50,18 @@
                 <td><?php echo classExists("2CL"); ?></td>
                 <td><?php echo classExists("2A"); ?></td>
                 <td><?php echo classExists("2B"); ?></td>
+                <td><?php echo classExists("2AB"); ?></td>
                 <td><?php echo classExists("2C"); ?></td>
                 <td><?php echo classExists("2D"); ?></td>
+                <td><?php echo classExists("2CD"); ?></td>
                 <td><?php echo classExists("2E"); ?></td>
                 <td><?php echo classExists("2F"); ?></td>
                 <td><?php echo classExists("2G"); ?></td>
+                <td><?php echo classExists("2FG"); ?></td>
                 <td><?php echo classExists("2H"); ?></td>
                 <td><?php echo classExists("2J"); ?></td>
                 <td><?php echo classExists("2K"); ?></td>
+                <td><?php echo classExists("2JK"); ?></td>
                 <td><?php echo classExists("2M"); ?></td>
                 <td><?php echo classExists("2MM"); ?></td>
                 <td><?php echo classExists("2N"); ?></td>
@@ -60,6 +72,7 @@
                 <td><?php echo classExists("3CL"); ?></td>
                 <td><?php echo classExists("3A"); ?></td>
                 <td><?php echo classExists("3B"); ?></td>
+                <td><?php echo classExists("3AB"); ?></td>
                 <td><?php echo classExists("3E"); ?></td>
                 <td><?php echo classExists("3H"); ?></td>
                 <td><?php echo classExists("3K"); ?></td>
@@ -72,12 +85,19 @@
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
             </tr>
             <tr>
                 <td class="class-heading">MISC</td>
                 <td><?php echo classExists("EX"); ?></td>
                 <td><?php echo classExists("NOV"); ?></td>
                 <td><?php echo classExists("METAL"); ?></td>
+                <td><?php echo classExists("MISC"); ?></td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
